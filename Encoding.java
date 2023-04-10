ae80010229dccc02cbd8954e43a9c290ea3fce1b
Simon <simon.cheremisin@gmail.com>
2023-04-10 07:17:19 +0300

    Вынес всю логику чтения из файла в отдельный метод takePassword.

--- Encoding.java
+++ Encoding.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Encoding {
+    private String line;
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    try(BufferedReader bufferedReader = new BufferedReader(new FileReader(scanner.nextLine()))){
-        String line = bufferedReader.readLine();
-        System.out.println(line);
-    }catch(IOException e){
-        e.printStackTrace();
+        Encoding encoding = new Encoding();
+        encoding.takePassword();
     }
+    private String takePassword(){
+        System.out.print("Введите путь расположения файла с паролем: ");
+        Scanner scanner = new Scanner(System.in);
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(scanner.nextLine()))){
+            line = bufferedReader.readLine();
+            System.out.println(line);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return line;
     }
 }
