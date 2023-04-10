63daea8c71c78fadd6d1f81b2a1be924c9a60717
Simon <92649440+krabat7@users.noreply.github.com>
2023-04-10 08:05:51 +0300

    Delete CryptoKey.java
    Неправильно закоммитил класс.

--- CryptoKey.java
+++
@@ -1,8 +0,0 @@
-public class CryptoKey {
-    // Число будет сдвигаться при шифровке/расишфровеке
-    // на numberToShift в криптографическом алфавите.
-    private static int numberToShift = 6;
-    public static int getNumberToShift(){
-        return numberToShift;
-    }
-}
