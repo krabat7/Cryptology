f6e78a504de2615cbe72c0940027b20937b26822
Simon <simon.cheremisin@gmail.com>
2023-04-10 20:33:39 +0300

    Поменял название переменной и название геттера.

--- CryptoKey.java
+++ CryptoKey.java
@@ -1,6 +1,6 @@
 public class CryptoKey {
     // Создание криптографического ключа,
     // в перменнной numberToShift хранится число для сдвига (Шифр Цезаря).
-    private static int numberToShift = 6;
-    public static int getNumberToShift(){ return numberToShift; }
+    private static int cryptoKey = 6;
+    public static int getCryptoKey(){ return cryptoKey; }
 }
