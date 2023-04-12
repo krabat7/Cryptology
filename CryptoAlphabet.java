c5c2e39c282f91c69d545f0d0803b0c8db037874
Simon <simon.cheremisin@gmail.com>
2023-04-12 20:33:39 +0300

    Добавлен ГЕТТЕР для получения алфавита. Добавлено отдельно поле ПРОБЕЛ, для реализации логики метода isPunctuationMarksOnRightPlace в классе BruteForce.

--- CryptoAlphabet.java
+++ CryptoAlphabet.java
@@ -1,10 +1,15 @@
 public class CryptoAlphabet {
     private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
-    private static final String PUNCTUATION_MARKS = ".,””:-!? ";
+    private static final String PUNCTUATION_MARKS = ".,””:-!?";
+    private static final String WHITE_SPACE = " ";
     public String getCryptoAlphabet(){
-        return ALPHABET + PUNCTUATION_MARKS;
+        return ALPHABET + PUNCTUATION_MARKS + WHITE_SPACE;
     }
     public String getPunctuationMarks(){
         return PUNCTUATION_MARKS;
+    }
+
+    public String getALPHABET() {
+        return ALPHABET;
     }
 }
